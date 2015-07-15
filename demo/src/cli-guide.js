5005fdd5cf184701eb0f51bf1d14901d2e4e4302
Willy Aguirre <marti1125@gmail.com>
2015-07-15 14:57:38 -0500

    add chef tutorial #93

--- demo/src/cli-guide.js
+++ demo/src/cli-guide.js
@@ -234,6 +234,7 @@
           $(this).removeAttr('contenteditable');
           effect.call($('<p id="'+id+'" class="response">').appendTo(self),handler(this.textContent || this.innerText));
 
+          // create prompt
           newline($(this).text());
 
           $("#"+id+".response").html(commands(opts.commandStepsFile,$(this).text()));
@@ -283,6 +284,156 @@
           // show preload files issue #62
           if($(this).text() == "ls") {
             $("#"+id+".response").html(localStorage.getItem("files").split(",").join(" "));
+          }
+
+          function removeItemFromArray(array, item){
+            for(var i in array){
+              if(array[i]==item){
+                array.splice(i,1);
+                break;
+              }
+            }
+          }
+
+          // delete file remote key from LocalStorage issue #81
+          if($(this).text().replace(/\s\s+/g,' ') == "rm -r " + $(this).text().split(" ").pop()) {
+            var fileName = $(this).text().split(" ").pop();
+            if(localStorage.getItem(fileName) != null){
+              var arrayFiles = localStorage.getItem("files").split(',');
+              arrayFiles = arrayFiles.filter(Boolean);
+              removeItemFromArray(arrayFiles, fileName);
+              localStorage.setItem("files",arrayFiles);
+              localStorage.removeItem(fileName);
+            }
+          }
+
+          if($(this).text().replace(/\s\s+/g,' ') == "git clone " + $(this).text().split(" ").pop()) {
+
+            $("#"+id+".response").html("");
+            $("#"+id+".response .objects").stop();
+
+            var url = $(this).text().split(" ").pop();
+            var httpGithub = "https://github.com/";
+            var git = ".git";
+            var httpGithubBoolean = false;
+            var gitBoolean = false;
+
+            if ( url.indexOf(httpGithub) > -1 ) {
+              httpGithubBoolean = true
+            }
+
+            if( url.indexOf(git) > -1 ){
+              gitBoolean = true
+            }
+            console.log(httpGithubBoolean);
+            console.log(gitBoolean);
+            var repoName= url.substring(url.lastIndexOf("/")+1,url.lastIndexOf(".git"));
+
+            if(httpGithubBoolean && gitBoolean){
+
+              $("#"+id+".response").append("Cloning into '"+repoName+"'... <br/>");
+              $("#"+id+".response").append("remote: Counting objects: 4643, done.<br/>");
+              $("#"+id+".response").append('remote: Compressing objects: '
+              + '<span id="objects" class="objects">100</span>'
+              + '% (<span id="objects_p" class="objects">12</span>/12), done.<br/>');
+
+              $("#"+id+".response #objects").html(100);
+              $("#"+id+".response #objects_p").html(12);
+
+              // animation
+              $("#"+id+".response .objects").each(function () {
+                $(this).prop('Counter',0).animate({
+                  Counter: $(this).text()
+                }, {
+                  duration: 900,
+                  easing: 'swing',
+                  step: function (now) {
+                    $(this).text(Math.ceil(now));
+                  }
+                });
+              });
+
+              $("#"+id+".response .objects").promise().done(function(){
+                $("#"+id+".response").append(
+                  '<span id="down_c" class="down_class">Receiving objects: <span id="down_m" class="down">100</span>%'
+                + ' (<span id="down_p" class="down">4643</span>/4643) '
+                + ' <span id="down_m" class="down">28</span> MiB | <span id="down_k" class="down">167</span> '
+                + ' Kib/s, done. </span><br/> '
+                );
+
+                $("#"+id+".response #down_m").html(100);
+                $("#"+id+".response #down_p").html(4643);
+                $("#"+id+".response #down_m").html(28);
+                $("#"+id+".response #down_k").html(167);
+
+                $("#"+id+".response .down").each(function () {
+                  $(this).prop('Counter',0).animate({
+                    Counter: $(this).text()
+                  }, {
+                    duration: 3000,
+                    easing: 'swing',
+                    step: function (now) {
+                      $(this).text(Math.ceil(now));
+                    }
+                  });
+                });
+
+                $("#"+id+".response .down").promise().done(function(){
+
+                  $("<span>remote: Total 4643 (delta 2), reused 0 (delta 0), pack-reused 4631</span><br/>").insertBefore("#"+id+".response #down_c.down_class");
+
+                  $("#"+id+".response").append('Resolving deltas: '
+                  + '<span id="delta" class="delta">100</span>'
+                  + '% (<span id="delta_p"class="delta">2961</span>/2961), done.<br/>');
+
+                  $("#"+id+".response #delta").html(100);
+                  $("#"+id+".response #delta_p").html(2961);
+
+                  $("#"+id+".response .delta").each(function () {
+                    $(this).prop('Counter',0).animate({
+                      Counter: $(this).text()
+                    }, {
+                      duration: 2000,
+                      easing: 'swing',
+                      step: function (now) {
+                        $(this).text(Math.ceil(now));
+                      }
+                    });
+                  });
+
+                  $("#"+id+".response .delta").promise().done(function(){
+
+                    $("#"+id+".response").append("Checking connectivity... done. <br/>");
+
+                    $("#"+id+".response").append('Checking out files: '
+                    + '<span id="files" class="files">100</span>'
+                    + '% (<span id="files_p"class="files">975</span>/975), done.<br/>');
+
+                    $("#"+id+".response #files").html(100);
+                    $("#"+id+".response #files_p").html(975);
+
+                    $("#"+id+".response .files").each(function () {
+                      $(this).prop('Counter',0).animate({
+                        Counter: $(this).text()
+                      }, {
+                        duration: 2000,
+                        easing: 'swing',
+                        step: function (now) {
+                          $(this).text(Math.ceil(now));
+                        }
+                      });
+                    });
+
+                  });
+
+                });
+
+              });
+
+            } else {
+              $("#"+id+".response").html("fatal: repository '"+url+"' does not exist");
+            }
+
           }
 
           return false;
@@ -410,7 +561,6 @@
           // prevent duplicate files
           var existDuplicate = true;
           for(var f = 0; f < arrayFiles.length; f++){
-            console.log(arrayFiles[f])
             if(arrayFiles[f] != $(this).text()){
               checkDuplicate = false;
             } else {
@@ -563,6 +713,7 @@
 
     var heightContent = heightContentParent;
 
+    $("#steps_section").css("height",opts.heightTerminal + "px");
     $("#editor-content-parent").css("height",heightContentParent + "px");
     $("#editor-content").css("height",heightContent + "px");
 
