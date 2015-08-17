15ac1f47645d861de507cf6a5f2e75aaddd2041f
Willy Aguirre <marti1125@gmail.com>
2015-08-16 20:25:56 -0500

    correct git clone validation

--- src/cli-guide.js
+++ src/cli-guide.js
@@ -708,52 +708,29 @@
           }
 
           // git clone
-          if(UtilRegExp.gitClone($(this).text().replace(/\s\s+/g,' '))){
+          if($(this).text().replace(/\s\s+/g,' ') == "git clone " + $(this).text().split(" ").pop()) {
 
-            var url = $(this).text().split(" ").pop();
-            var repoName= url.substring(url.lastIndexOf("/")+1,url.lastIndexOf(".git"));
-            var input = $(this).text().replace(/\s\s+/g,' ');
+            if(UtilRegExp.gitClone($(this).text().replace(/\s\s+/g,' '))){
 
-            $("#"+id+".response").append("Cloning into '"+repoName+"'... <br/>");
-            $("#"+id+".response").append("remote: Counting objects: 4643, done.<br/>");
-            $("#"+id+".response").append('remote: Compressing objects: '
-            + '<span id="objects" class="objects">100</span>'
-            + '% (<span id="objects_p" class="objects">12</span>/12), done.<br/>');
+              var url = $(this).text().split(" ").pop();
+              var repoName= url.substring(url.lastIndexOf("/")+1,url.lastIndexOf(".git"));
+              var input = $(this).text().replace(/\s\s+/g,' ');
 
-            $("#"+id+".response #objects").html(100);
-            $("#"+id+".response #objects_p").html(12);
+              $("#"+id+".response").append("Cloning into '"+repoName+"'... <br/>");
+              $("#"+id+".response").append("remote: Counting objects: 4643, done.<br/>");
+              $("#"+id+".response").append('remote: Compressing objects: '
+              + '<span id="objects" class="objects">100</span>'
+              + '% (<span id="objects_p" class="objects">12</span>/12), done.<br/>');
 
-            // animation
-            $("#"+id+".response .objects").each(function () {
-              $(this).prop('Counter',0).animate({
-                Counter: $(this).text()
-              }, {
-                duration: 900,
-                easing: 'swing',
-                step: function (now) {
-                  $(this).text(Math.ceil(now));
-                }
-              });
-            });
+              $("#"+id+".response #objects").html(100);
+              $("#"+id+".response #objects_p").html(12);
 
-            $("#"+id+".response .objects").promise().done(function(){
-              $("#"+id+".response").append(
-                '<span id="down_c" class="down_class">Receiving objects: <span id="down_m" class="down">100</span>%'
-              + ' (<span id="down_p" class="down">4643</span>/4643) '
-              + ' <span id="down_mb" class="down">28</span> MiB | <span id="down_k" class="down">167</span> '
-              + ' Kib/s, done. </span><br/> '
-              );
-
-              $("#"+id+".response #down_m").html(100);
-              $("#"+id+".response #down_p").html(4643);
-              $("#"+id+".response #down_mb").html(28);
-              $("#"+id+".response #down_k").html(167);
-
-              $("#"+id+".response .down").each(function () {
+              // animation
+              $("#"+id+".response .objects").each(function () {
                 $(this).prop('Counter',0).animate({
                   Counter: $(this).text()
                 }, {
-                  duration: 3000,
+                  duration: 900,
                   easing: 'swing',
                   step: function (now) {
                     $(this).text(Math.ceil(now));
@@ -761,22 +738,24 @@
                 });
               });
 
-              $("#"+id+".response .down").promise().done(function(){
+              $("#"+id+".response .objects").promise().done(function(){
+                $("#"+id+".response").append(
+                  '<span id="down_c" class="down_class">Receiving objects: <span id="down_m" class="down">100</span>%'
+                + ' (<span id="down_p" class="down">4643</span>/4643) '
+                + ' <span id="down_mb" class="down">28</span> MiB | <span id="down_k" class="down">167</span> '
+                + ' Kib/s, done. </span><br/> '
+                );
 
-                $("<span>remote: Total 4643 (delta 2), reused 0 (delta 0), pack-reused 4631</span><br/>").insertBefore("#"+id+".response #down_c.down_class");
+                $("#"+id+".response #down_m").html(100);
+                $("#"+id+".response #down_p").html(4643);
+                $("#"+id+".response #down_mb").html(28);
+                $("#"+id+".response #down_k").html(167);
 
-                $("#"+id+".response").append('Resolving deltas: '
-                + '<span id="delta" class="delta">100</span>'
-                + '% (<span id="delta_p"class="delta">2961</span>/2961), done.<br/>');
-
-                $("#"+id+".response #delta").html(100);
-                $("#"+id+".response #delta_p").html(2961);
-
-                $("#"+id+".response .delta").each(function () {
+                $("#"+id+".response .down").each(function () {
                   $(this).prop('Counter',0).animate({
                     Counter: $(this).text()
                   }, {
-                    duration: 2000,
+                    duration: 3000,
                     easing: 'swing',
                     step: function (now) {
                       $(this).text(Math.ceil(now));
@@ -784,18 +763,18 @@
                   });
                 });
 
-                $("#"+id+".response .delta").promise().done(function(){
+                $("#"+id+".response .down").promise().done(function(){
 
-                  $("#"+id+".response").append("Checking connectivity... done. <br/>");
+                  $("<span>remote: Total 4643 (delta 2), reused 0 (delta 0), pack-reused 4631</span><br/>").insertBefore("#"+id+".response #down_c.down_class");
 
-                  $("#"+id+".response").append('Checking out files: '
-                  + '<span id="files" class="files">100</span>'
-                  + '% (<span id="files_p"class="files">975</span>/975), done.<br/>');
+                  $("#"+id+".response").append('Resolving deltas: '
+                  + '<span id="delta" class="delta">100</span>'
+                  + '% (<span id="delta_p"class="delta">2961</span>/2961), done.<br/>');
 
-                  $("#"+id+".response #files").html(100);
-                  $("#"+id+".response #files_p").html(975);
+                  $("#"+id+".response #delta").html(100);
+                  $("#"+id+".response #delta_p").html(2961);
 
-                  $("#"+id+".response .files").each(function () {
+                  $("#"+id+".response .delta").each(function () {
                     $(this).prop('Counter',0).animate({
                       Counter: $(this).text()
                     }, {
@@ -807,20 +786,45 @@
                     });
                   });
 
-                  $("#"+id+".response .files").promise().done(function(){
-                    // save this command in the history
-                    newline(input);
+                  $("#"+id+".response .delta").promise().done(function(){
+
+                    $("#"+id+".response").append("Checking connectivity... done. <br/>");
+
+                    $("#"+id+".response").append('Checking out files: '
+                    + '<span id="files" class="files">100</span>'
+                    + '% (<span id="files_p"class="files">975</span>/975), done.<br/>');
+
+                    $("#"+id+".response #files").html(100);
+                    $("#"+id+".response #files_p").html(975);
+
+                    $("#"+id+".response .files").each(function () {
+                      $(this).prop('Counter',0).animate({
+                        Counter: $(this).text()
+                      }, {
+                        duration: 2000,
+                        easing: 'swing',
+                        step: function (now) {
+                          $(this).text(Math.ceil(now));
+                        }
+                      });
+                    });
+
+                    $("#"+id+".response .files").promise().done(function(){
+                      // save this command in the history
+                      newline(input);
+                    });
+
                   });
 
                 });
 
               });
 
-            });
+            } else {
+              $("#"+id+".response").html("fatal: repository '"+$(this).text().replace(/\s\s+/g,' ')+"' does not exist");
+            }
 
-          } else {
-            $("#"+id+".response").html("fatal: repository '"+$(this).text().replace(/\s\s+/g,' ')+"' does not exist");
-          }          
+          }
 
           var inputUser = $(this).text();
           if($("#"+id+".cline").length > 0){
