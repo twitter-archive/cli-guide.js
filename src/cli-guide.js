18ae74a4cc6e0930453930758ce7524e7b9384d6
Willy Aguirre <marti1125@gmail.com>
2015-09-14 12:43:37 -0500

    added UtilRegExp.language for get the extension of the language

--- src/cli-guide.js
+++ src/cli-guide.js
@@ -423,8 +423,47 @@
         // for testing this regular expression you can use
         // https://regex101.com/
         gitClone: function(text){
-          var gitclone = /git\sclone\s(https:\/\/|git:\/\/)([a-zA-Z-.-_--]{0,}[^\//]\/){0,}[a-zA-Z-.-_--]{0,}(\.git|\.GIT)/
+          var gitclone = /git\sclone\s(https:\/\/|git:\/\/)([a-zA-Z-.-_--]{0,}[^\//]\/){0,}[a-zA-Z-.-_--]{0,}(\.git|\.GIT)/;
           return gitclone.test(text);
+        },
+        language: function(filename){
+          var css = /\.css/;
+          var js = /\.js/;
+          var py = /\.py/;
+          var go = /\.go/;
+          var java = /\.java/;
+          var scala = /\.scala/;
+          var ruby = /\.rb/;
+          var rust = /\.rs/;
+          var haml = /\.haml/;
+          if(css.test(filename)){
+            return "css";
+          }
+          if(js.test(filename)){
+            return "javascript";
+          }
+          if(py.test(filename)){
+            return "python";
+          }
+          if(go.test(filename)){
+            return "go";
+          }
+          if(java.test(filename)){
+            return "java";
+          }
+          if(scala.test(filename)){
+            return "scala";
+          }
+          if(ruby.test(filename)){
+            return "ruby";
+          }
+          if(rust.test(filename)){
+            return "rust";
+          }
+          if(haml.test(filename)){
+            return "haml";
+          }
+          return "markup";
         }
       };
 
@@ -770,7 +809,7 @@
               '<pre><code id="lang" class="language-'+file.language+'">'
               +'</code></pre>'
             );
-            $('#lang').html(file.content);
+            $('#lang').html(file.content.split("<br>").join("\n"));
             Prism.highlightElement($('#lang')[0]);
             // show the name of the file in header
             $('#editor-header-filename').html("File: " + filename);
@@ -1050,11 +1089,12 @@
                 language: file.language
               }));
           } else {
+            console.log($("#editor-content").html());
             // save a new file
             localStorage.setItem($(this).text(),
               JSON.stringify({
                 content: $("#editor-content").html(),
-                language: "markup"
+                language: UtilRegExp.language($(this).text())
               }));
           }
           $("#editor").hide();
