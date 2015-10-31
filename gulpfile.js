8b2af0d67e97d80f924a71e74f30958ddb9c5efb
= <=>
2015-10-31 23:49:58 +0530

    Add paths variable in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,38 @@ var gulp       = require('gulp'),
     rename     = require('gulp-rename'),
     minifyCss  = require('gulp-minify-css'),
     jshint     = require('gulp-jshint'),
-    uglify     = require('gulp-uglify');
+    uglify     = require('gulp-uglify'),
+    paths = {
+        scripts: 'src/*.js',
+        styles: 'src/*.css',
+        fonts: '*.{ttf, otf}',
+        res: ['package.json','README.md','LICENSE','nano.gif','terminal.gif']
+    };
 
 gulp.task('publish', function() {
-  return gulp.src(['package.json','README.md','LICENSE','nano.gif','terminal.gif'])
+  return gulp.src(paths.res)
          .pipe(gulp.dest('dist'));
 });
 
 gulp.task('copy-original-files', function() {
-  return gulp.src('src/*.{css,js,ttf,otf}')
+  return gulp.src([paths.scripts, paths.styles, paths.fonts])
          .pipe(gulp.dest('dist'));
 });
 
 gulp.task('minify-css', function() {
-  return gulp.src('src/*.css')
+  return gulp.src(paths.styles)
          .pipe(minifyCss())
          .pipe(gulp.dest('dist'));
 });
 
 gulp.task('lint', function() {
-  return gulp.src(['src/*.js'])
+  return gulp.src([paths.scripts])
          .pipe(jshint())
          .pipe(jshint.reporter('default'));
 });
 
 gulp.task('compress-js', function() {
-  return gulp.src('src/*.js')
+  return gulp.src(paths.scripts)
          .pipe(uglify())
          .pipe(rename({suffix: '.min'}))
          .pipe(gulp.dest('dist'))
